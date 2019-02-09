14882758f03210f520e9fce405d8f03ada18c27f
sudo <wadkar@gh>
2019-02-09 14:36:10 +0530

    Solution 5 - State management (manual)

--- src/exercise/05-State.js
+++ src/exercise/05-State.js
@@ -16,7 +16,7 @@ class Counter extends Component {
          * Initialize a state here with initial value of counter set to 0
          * this.state = { counter: defaultValue }
          */
-        this.state = {};
+        this.state = { counter: 1 };
 
         /**
          * 💡 
@@ -43,6 +43,9 @@ class Counter extends Component {
          *              return newState 
          *      });         
          */
+        this.setState(currentState => {
+            return { counter: currentState.counter + 1 }
+        })
     }
 
     /**
@@ -61,6 +64,9 @@ class Counter extends Component {
          *              return newState 
          *      });
          */
+        this.setState(currentState => {
+            return {counter: currentState.counter - 1 }
+        })
     }
 
     render() {
