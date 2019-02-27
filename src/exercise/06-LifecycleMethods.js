91e28018ec82192ba2b1edff97db0aeaf9a56733
sudo <wadkar@gh>
2019-02-27 15:01:49 +0530

    Solution 6 - Component Lifecycle Methods

--- src/exercise/06-LifecycleMethods.js
+++ src/exercise/06-LifecycleMethods.js
@@ -43,7 +43,12 @@ class CompanyProfile extends Component {
      *             .catch(error => console.log(error))
      * */
     componentDidMount() {
-
+        DataAPI.getCompanyProfile(this.props.stockTicker)
+        .then(profile => {
+            this.setState({
+                companyProfileInfo: profile
+            })
+        });
     }
 
     render() {
